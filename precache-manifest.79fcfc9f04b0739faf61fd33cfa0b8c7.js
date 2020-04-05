d8573a370067e6769390f500554d40d434ca02d3
Mahipas Amala Renush <mahirenush@gmail.com>
2020-04-05 19:20:33 +0530

    Updates

---
+++ precache-manifest.79fcfc9f04b0739faf61fd33cfa0b8c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622803e873a9f683c1b72c5d12c63c9c",
+    "url": "/eats-react/index.html"
+  },
+  {
+    "revision": "c8d6486a29f8c73506f9",
+    "url": "/eats-react/static/css/2.ee95de2a.chunk.css"
+  },
+  {
+    "revision": "b3f79510da246fc1ba27",
+    "url": "/eats-react/static/css/main.6c89e37b.chunk.css"
+  },
+  {
+    "revision": "c8d6486a29f8c73506f9",
+    "url": "/eats-react/static/js/2.49cada4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eats-react/static/js/2.49cada4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f79510da246fc1ba27",
+    "url": "/eats-react/static/js/main.478b8b08.chunk.js"
+  },
+  {
+    "revision": "ff4eae32b8a25156ff53",
+    "url": "/eats-react/static/js/runtime-main.aa3fd0ee.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/eats-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/eats-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/eats-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/eats-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/eats-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eats-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
